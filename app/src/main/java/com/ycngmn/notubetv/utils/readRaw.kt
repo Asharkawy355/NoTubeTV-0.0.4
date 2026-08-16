dda6f22bb624f8ddb5780c0348830615511ad1f4
Asharkawy355 <101805669+Asharkawy355@users.noreply.github.com>
2026-08-16 18:23:05 +0300

    Fix formatting in readRaw function

--- app/src/main/java/com/ycngmn/notubetv/utils/readRaw.kt
+++ app/src/main/java/com/ycngmn/notubetv/utils/readRaw.kt
@@ -3,6 +3,5 @@ package com.ycngmn.notubetv.utils
 import android.content.Context
 
 fun readRaw(context: Context, resId: Int): String {
-    return context.resources.openRawResource(resId)
-        .bufferedReader().use { it.readText() }
+    return context.resources.openRawResource(resId).bufferedReader().use { it.readText() }
 }
