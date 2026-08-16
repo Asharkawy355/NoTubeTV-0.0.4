3f4711e6c0b5d369f5941c2dd9dec8e01fa4628a
Asharkawy355 <101805669+Asharkawy355@users.noreply.github.com>
2026-08-16 18:22:23 +0300

    Refactor ExitBridge to add exit method

--- app/src/main/java/com/ycngmn/notubetv/utils/ExitBridge.kt
+++ app/src/main/java/com/ycngmn/notubetv/utils/ExitBridge.kt
@@ -3,7 +3,9 @@ package com.ycngmn.notubetv.utils
 import android.webkit.JavascriptInterface
 import androidx.compose.runtime.MutableState
 
-class ExitBridge(val exitTrigger: MutableState<Boolean>) {
+class ExitBridge(private val exitTrigger: MutableState<Boolean>) {
     @JavascriptInterface
-    fun onExitCalled() { exitTrigger.value = true }
+    fun exit() {
+        exitTrigger.value = true
+    }
 }
